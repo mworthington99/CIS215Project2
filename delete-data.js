559a56314b939cfe31f631686f581b5db8e669c5
callen15 <callen4280@icloud.com>
2025-04-21 13:50:20 -0400

    removed DOM CONTENT LOADED may be delaying js

--- delete-data.js
+++ delete-data.js
@@ -16,50 +16,48 @@
  * 
  */
 console.log("delete-data.js loaded!")
-function delete_data(){
+
+/**
+ * Best practice to ensure DOMContent is loaded to ensure html is loaded.
+ * Even though it should be loaded already given the script tag is called
+ * on @line 244 in @file project1data.php
+ */
+
+    const form = document.getElementById("delete-data-form");  
     /**
-     * Best practice to ensure DOMContent is loaded to ensure html is loaded.
-     * Even though it should be loaded already given the script tag is called
-     * on @line 244 in @file project1data.php
+     * Below is an event listener that will trigger when the
+     * submit button is pressed. 
      */
-    document.addEventListener("DOMContentLoaded", function(){   
-        const form = document.getElementById("delete-data-form");  
+    form.addEventListener("submit", function(event){
+        event.preventDefault();
         /**
-         * Below is an event listener that will trigger when the
-         * submit button is pressed. 
-         */
-        form.addEventListener("submit", function(event){
-            event.preventDefault();
-            /**
-             * @constant email will grab the email from the html form. 
-             * @constant responseMessage is initiallizing the response on the webform
-             * to let the user know the status of there data. i.e.
-             * "Successfully Deleted", "Error Occured"
-             * @constant xhr shortened accronym for Xml-Https-Request
-             * */
+         * @constant email will grab the email from the html form. 
+         * @constant responseMessage is initiallizing the response on the webform
+         * to let the user know the status of there data. i.e.
+         * "Successfully Deleted", "Error Occured"
+         * @constant xhr shortened accronym for Xml-Https-Request
+         * */
 
-            const email = document.getElementById("email-id").value;
-            const responseMessage = document.getElementById("ResponseMessage");
-            console.log("before");
-            const xhr = new XMLHttpRequest();
-            xhr.open("POST", "delete_data.php");
-            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");     
-            xhr.send("email=" + encodeURIComponent(email));
-            console.log("-after");
-            xhr.onload = function() {
-                if (xhr.status === 200) {
-                    console.log("xhr.status === 200");
-                    responseMessage.textContent = xhr.responseText;
-                }else{
-                    console.log("Nothing");
-                    responseMessage.textContent = "Error: Could not connect to SQL Server on CSN Linux"
-                }
+        const email = document.getElementById("email-id").value;
+        const responseMessage = document.getElementById("ResponseMessage");
+        console.log("before");
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "delete_data.php");
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");     
+        xhr.send("email=" + encodeURIComponent(email));
+        console.log("-after");
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                console.log("xhr.status === 200");
+                responseMessage.textContent = xhr.responseText;
+            }else{
+                console.log("Nothing");
+                responseMessage.textContent = "Error: Could not connect to SQL Server on CSN Linux"
             }
+        }
 
-        });
     });
-}
-document.addEventListener("DOMContentLoaded", function(){
-    delete_data();
-});
+
+
+
     
