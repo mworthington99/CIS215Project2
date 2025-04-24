a98dc036df4c36a62db5cd999676abff963f9357
callen15 <callen4280@icloud.com>
2025-04-23 21:14:30 -0400

    Password JS bug fixed

--- password_validation.js
+++ password_validation.js
@@ -31,16 +31,13 @@ function password_validation() {
             const password = password_input.value;
             let messages = []; //array object for all messages that can be displayed for errors in password strength.
 
-            if(password < 8) messages.push("Password must be at least 8 charcters");
+            if(password.length < 8) messages.push("Password must be at least 8 charcters");
             if(!/[a-z]/.test(password)) messages.push("Password must have at least one lowercase letter"); //regex explenation [a-z] all lowercase letters in regex
             if(!/[A-Z]/.test(password)) messages.push("Password must have at least one uppercase letter"); //regex explenation [A-Z] all uppercase letters in regex
-            //regex explenation all of the special charcters when you press shift for 1 through 9 on your keyboard
-            if(!/[!@#$%^&*(]/.test(password)) messages.push("Password must have at least one special charcter from *shift 1-9 on keyboard");
-            //Triple equals still means strict equality, meaning it will match type and value. 
+            if(!/[!@#$%^&*()]/.test(password)) messages.push("Password must have at least one special charcter from *shift 1-9 on keyboard");
             //messages.lenght (int) to 0 (int). I know I could probably use == here
-            //but I just want as much review as possible when writing ===. 
             if (messages.length === 0){
-                password_status = "Password has met all criteria";
+                password_status.textContent = "Password has met all criteria";
                 password_status.style.color = "green"; //changes the text to green. CSS
             }
             else {
